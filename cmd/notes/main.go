8b65fc9817643008277cf6c189e3ab0766578ede
Alex Musayev <alex.musayev@gmail.com>
2026-04-04 19:03:19 +0200

    Reset SIGPIPE to default for clean pipe behavior

--- cmd/notes/main.go
+++ cmd/notes/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "github.com/dreikanter/notescli/internal/cli"
+import (
+	"os/signal"
+	"syscall"
+
+	"github.com/dreikanter/notescli/internal/cli"
+)
 
 func main() {
+	// Let the OS terminate the process on SIGPIPE (conventional
+	// behavior for CLI tools piped through head, less, etc.).
+	signal.Reset(syscall.SIGPIPE)
 	cli.Execute()
 }
